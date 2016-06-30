7d8ab73dd44de5f52df10a55891005c28d5b5f38
Joseph Luck <joseph.luck@sky.com>
2016-06-30 21:57:45 +0100

    Beginnings of nesting resource section in model form

--- scripts/containers/ResourceEdit.js
+++ scripts/containers/ResourceEdit.js
@@ -84,57 +84,7 @@ class ResourceEdit extends Component {
 							{"Save"}
 						</a>
 					</div>
-
-					<div className="flex-1 overflow-auto">
-						<div className="box without-bottom-padding">
-							<div className="input-label">{"Name"}</div>
-							<input value={this.state.resource.name}
-								onChange={(e) => {
-									var value = e.target.value.replace(/\W+/g, " ").replace(/ /g,"_");
-									this.saveValue('name', value);
-								}}>
-							</input>
-							<div className="input-label">{"Type"}</div>
-							<select value={this.state.resource.type}
-								onChange={(e) => {
-									this.saveValue('type', e.target.value);
-								}}>
-								<option value={"array"}>{"Array"}</option>
-								<option value={"singular"}>{"Signular"}</option>
-							</select>
-							{this.state.resource.type === 'array' ?
-								<div>
-									<div className="input-label">{"Length"}</div>
-									<input value={this.state.resource.length}
-										onChange={(e) => {
-											let value = 0;
-
-											if (!isNaN(parseFloat(e.target.value)) && isFinite(e.target.value)) {
-												value = parseFloat(e.target.value);
-											}
-											this.saveValue('length', value);
-										}}>
-									</input>
-								</div>
-								: null
-							}
-						</div>
-
-						<div className="section-title with-top-border flex">
-							<span className="flex-1">
-								{`${this.state.resource.name} model description`}
-							</span>
-							<a href=""
-								onClick={(e) => {
-									e.preventDefault();
-									this.addAnotherKey();
-								}}>
-								{"Add another key"}
-							</a>
-						</div>
-
-						<ModelForm resource={this.state.resource}></ModelForm>
-					</div>
+					<ModelForm resource={this.state.resource}></ModelForm>
 				</div>
 				{this.props.children}
 			</div>
